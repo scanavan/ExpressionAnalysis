fe193be72c7c6dd35940b07189dfe08fa163fca4
scanavan <scanavan@usf.edu>
2017-10-02 13:38:18 -0400

    Test code in main to load image from file, create window to display image, display image, and wait for user input to keep window up before exiting.

---
+++ DataPreProcessing/source/main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+	//load color image from file
+	cv::Mat image = cv::imread("images/000.jpg", cv::IMREAD_COLOR);
+	if (image.empty())
+	{
+		std::cout << "Failed to load image." << std::endl;
+	}
+	//create window to show image on
+	cv::namedWindow("Test", cv::WINDOW_AUTOSIZE);
+	//show image
+	cv::imshow("Test", image);
+	//wait for user input so window doesn't close
+	cv::waitKey(0);
+	return EXIT_SUCCESS;
+}
