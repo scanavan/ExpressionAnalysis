df22a878c51af106428cf7fb0ba8d260d1ab52f6
Astha Sharma <asthasharma@mail.usf.edu>
2017-12-15 00:15:32 -0500

    Changes for cropping images inside branched folders.

--- DataPreProcessing/source/main.cpp
+++ DataPreProcessing/source/main.cpp
@@ -11,11 +11,13 @@
 using namespace std;
 using namespace cv;
 
-void detectAndDisplay(Mat frame, int x, int y, string folderName, string imgName);
+void detectAndDisplay(Mat frame, int x, int y, string firstFloderName, string secondFolderName, string imgName);
 
 string face_cascade_name = "C:/Users/asthasharma017/Development/FacialExpressionRecognitionProjectWorkspace/ThirdParty/opencv/sources/samples/winrt_universal/VideoCaptureXAML/video_capture_xaml/video_capture_xaml.Windows/Assets/haarcascade_frontalface_alt.xml";
 CascadeClassifier face_cascade;
 string window_name = "Capture - Face detection";
+string sBasePath = "E:\\MMSE\\first 10 subjects 2D\\";
+string sCroppedImagePath = "E:\\MMSE\\cropped\\";
 int filenumber;
 string filename;
 
@@ -25,65 +27,69 @@ int main(void)
 		printf("--(!)Error loading\n");
 		return (-1);
 	}
-
-	WIN32_FIND_DATA FindFileData, file_data;
-	HANDLE hFind, dir;
-	string sPath;
-	vector<string> MyVect;
-	sPath.assign("images\\F005\\*");
-	hFind = FindFirstFile(sPath.data(), &FindFileData);
+	WIN32_FIND_DATA findFileDataFirst;
+	HANDLE hFindFirst;
+	string sPathFirst;
+	vector<string> MyVectFirst;
+	sPathFirst.assign(sBasePath + "*");
+	hFindFirst = FindFirstFile(sPathFirst.data(), &findFileDataFirst);
 	do
 	{
-		if (FindFileData.dwFileAttributes == 16)
+		if (findFileDataFirst.dwFileAttributes == 16)
 		{
-			MyVect.push_back(FindFileData.cFileName);
+			MyVectFirst.push_back(findFileDataFirst.cFileName);
 		}
-	} while (FindNextFile(hFind, &FindFileData));
-	FindClose(hFind);
-	for (int i = 0; i < MyVect.size(); i++) {
-		cout << MyVect.at(i) << endl;
-
-		
-
-		std::string pattern("images\\F005\\" + MyVect.at(i));
-		pattern.append("\\*");
-		WIN32_FIND_DATA data;
-		HANDLE hFind;
-		if ((hFind = FindFirstFile(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE) {
-			do {
-				cout << data.cFileName << endl;
-
-				Mat frame = imread("images/F005/" + MyVect.at(i) + "/" + data.cFileName);
-				cout << "images/F005/" + MyVect.at(i) + "/" + data.cFileName << endl;
-
-				if (!frame.empty()) {
-					detectAndDisplay(frame, 48, 48, MyVect.at(i), data.cFileName);
-				}
-				else {
-					printf(" --(!) No captured frame -- Break!");
-				}
-
-				int c = waitKey(10);
-
-				if (27 == char(c)) {
-					//break;
-				}
-
-
-			} while (FindNextFile(hFind, &data) != 0);
-			FindClose(hFind);
+	} while (FindNextFile(hFindFirst, &findFileDataFirst));
+	FindClose(hFindFirst);
+	for (int i = 0; i < MyVectFirst.size(); i++) {
+		if (MyVectFirst.at(i) == "." || MyVectFirst.at(i) == "..") {
+			continue;
 		}
-
-
-
+		std::string sPathSecond(sBasePath + MyVectFirst.at(i));
+		sPathSecond.append("\\*");
+		WIN32_FIND_DATA findFileDataSecond;
+		HANDLE hFindSecond;
+		vector<string> MyVectSecond;
+		hFindSecond = FindFirstFile(sPathSecond.data(), &findFileDataSecond);
+		do
+		{
+			if (findFileDataSecond.dwFileAttributes == 16)
+			{
+				MyVectSecond.push_back(findFileDataSecond.cFileName);
+			}
+		} while (FindNextFile(hFindSecond, &findFileDataSecond));
+		FindClose(hFindSecond);
+		for (int j = 0; j < MyVectSecond.size(); j++) {
+			if (MyVectSecond.at(i) == "." || MyVectSecond.at(i) == "..") {
+				continue;
+			}
+			std::string sPathImage(sBasePath + MyVectFirst.at(i) + "\\" + MyVectSecond.at(j));
+			sPathImage.append("\\*");
+			WIN32_FIND_DATA imageData;
+			HANDLE hFindImage;
+			if ((hFindImage = FindFirstFile(sPathImage.c_str(), &imageData)) != INVALID_HANDLE_VALUE) {
+				do {
+					Mat frame = imread(sBasePath + MyVectFirst.at(i) + "\\" + MyVectSecond.at(j) + "\\" + imageData.cFileName);
+					cout << sBasePath + MyVectFirst.at(i) + "\\" + MyVectSecond.at(j) + "\\" + imageData.cFileName << endl;
+					if (!frame.empty()) {
+						detectAndDisplay(frame, 48, 48, MyVectFirst.at(i), MyVectSecond.at(j), imageData.cFileName);
+					}
+					else {
+						cout << " --(!) No captured frame -- Break!" << endl;
+					}
+					int c = waitKey(10);
+					if (27 == char(c)) {
+						//break;
+					}
+				} while (FindNextFile(hFindImage, &imageData) != 0);
+				FindClose(hFindImage);
+			}
+		}
 	}
-
-	
-
 	return 0;
 }
 
-void detectAndDisplay(Mat frame, int x, int y, string folderName, string imgName)
+void detectAndDisplay(Mat frame, int x, int y, string firstFolderName, string secondFolderName, string imgName)
 {
 	std::vector<Rect> faces;
 	Mat frame_gray;
@@ -135,7 +141,7 @@ void detectAndDisplay(Mat frame, int x, int y, string folderName, string imgName
 		}
 
 		crop = frame(roi_b);
-		resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR); 
+		resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR);
 		cvtColor(crop, gray, CV_BGR2GRAY); // Convert cropped image to Grayscale
 
 		filename = "";
@@ -143,8 +149,6 @@ void detectAndDisplay(Mat frame, int x, int y, string folderName, string imgName
 		ssfn << filenumber << ".jpg";
 		filename = ssfn.str();
 		filenumber++;
-
-		//imwrite(filename, gray);
 
 		Point pt1(faces[ic].x, faces[ic].y); // Display detected faces on main window
 		Point pt2((faces[ic].x + faces[ic].height), (faces[ic].y + faces[ic].width));
@@ -156,13 +160,11 @@ void detectAndDisplay(Mat frame, int x, int y, string folderName, string imgName
 	text = sstm.str();
 
 	putText(frame, text, cvPoint(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
-	//imshow("original", frame);
 
 	if (!crop.empty())
 	{
-		//imshow("detected", crop);
 		resize(crop, crop, Size(x, y));
-		imwrite("images/cropped/F005_" + folderName + "_" + imgName + ".jpg", crop);
+		imwrite(sCroppedImagePath + firstFolderName + "_" + secondFolderName + "_" + imgName + ".jpg", crop);
 		cv::waitKey();
 	}
 	else
